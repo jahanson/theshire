9a376ac917e2e31958ded966e02863124393154e
Joseph Hanson <joe@veri.dev>
2024-07-25 18:21:21 -0500

    clean up

--- shell.nix
+++ shell.nix
@@ -18,25 +18,4 @@ pkgs.mkShell {
     pre-commit
     sops
   ];
-  # Possible inputs needed. Keeping here for posterity
-  # age
-  # ansible
-  # cilium-cli
-  # direnv
-  # derailed/k9s/k9s
-  # fluxcd/tap/flux
-  # go-task/tap/go-task
-  # helm
-  # ipcalc
-  # jq
-  # kubernetes-cli
-  # kustomize
-  # pre-commit
-  # prettier
-  # shellcheck
-  # sops
-  # stern
-  # talhelper
-  # yamllint
-  # yq
 }
